b3d747db7cc0cd4c189226c375ab647eeaa09740
Angie2BR <abrumfield14@yahoo.com>
2022-04-08 00:59:23 -0500

    Add files via upload

---
+++ hangman/js/modules/how.js
@@ -0,0 +1,16 @@
+import {sound} from './../data/sound.js';
+const How=(_ => {
+    const init=_=>{
+       // render();
+        //listeners();
+        console.log('how module');
+    }
+    
+    return{
+        init
+    }
+    
+    })();
+    
+    export default How;
+    
